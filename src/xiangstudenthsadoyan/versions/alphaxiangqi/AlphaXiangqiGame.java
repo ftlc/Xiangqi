4653f1d13a658b413929885d7ced15acb0bdae69
Harry Sadoyan <harrysadoyan@gmail.com>
2017-02-07 23:56:54 -0500

    Alpha

--- src/xiangstudenthsadoyan/versions/alphaxiangqi/AlphaXiangqiGame.java
+++ src/xiangstudenthsadoyan/versions/alphaxiangqi/AlphaXiangqiGame.java
@@ -6,19 +6,40 @@ import xiangqi.common.*;
  * Created by gnomeftlc on 2/7/17.
  */
 public class AlphaXiangqiGame implements XiangqiGame {
-    public AlphaXiangqiGame(){}
+    XiangqiPiece piece;
+    public AlphaXiangqiGame(){
+        piece = XiangqiPieceImp.makePiece(XiangqiPieceType.NONE, XiangqiColor.NONE);
+    }
+    private int movecount = 0;
+    private boolean valid;
+
+    private boolean isValid(){
+        return valid;
+    }
+    private void setValid(boolean v){
+        this.valid = v;
+    }
     @Override
     public MoveResult makeMove(XiangqiCoordinate source, XiangqiCoordinate destination) {
-        return null;
+        if(destination.getRank() != 1 || destination.getFile() != 2 || source.getFile() != 1 || source.getRank() != 1){
+            setValid(false);
+            return MoveResult.ILLEGAL;
+        }
+        if(movecount == 0) {
+            movecount++;
+            return MoveResult.OK;
+        } else {
+            return MoveResult.RED_WINS;
+        }
     }
 
     @Override
     public String getMoveMessage() {
-        return null;
+        return isValid() ? null : "Invalid Move";
     }
 
     @Override
     public XiangqiPiece getPieceAt(XiangqiCoordinate where, XiangqiColor aspect) {
-        return null;
+        return piece;
     }
 }
